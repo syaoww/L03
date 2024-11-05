2b03c80a3efd23c044e8e72984475088f3aac0bc
syaoww <21027127@myrp.edu.sg>
2024-11-05 22:00:28 +0800

    finished the exercises

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, TextInput, TouchableOpacity, Alert, ToastAndroid } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const InputBox = ({label, onChangeText})=> {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={onChangeText} />
+        </View>
+    );
+};
+
+const myApp = ()=> {
+    const [type, setValue] = useState('');
+    const [name, setName] = useState('');
+    const [pw, setPw] = useState('');
+    return (
+      <View>
+          <Text>User Type:</Text>
+          <RNPickerSelect
+              onValueChange={(value) => setValue((value))}
+              items={[
+                  { label: 'Admin', value: 'Admin' },
+                  { label: 'Guest', value: 'Guest' },
+              ]}
+          />
+          <InputBox label="User Name:" onChangeText={(text) => setName(text)}/>
+          <InputBox label="Password:" onChangeText={(text) => setPw(text)}/>
+          <TouchableOpacity onPress={()=> {
+              const correctPassword = '123';
+              let mymessage = 'Error! Wrong Password!';
+              if (pw == correctPassword) {
+                  mymessage = 'Welcome ' + type + ' ' + name;
+              }
+              ToastAndroid.show(mymessage, ToastAndroid.SHORT);
+          }
+          }>
+              <Text>LOG IN</Text>
+          </TouchableOpacity>
+          <Text>{pw}</Text>
+      </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default myApp;
